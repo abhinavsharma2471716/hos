514c2f0e855943826951a7fb008ab57d6cfae3ad
arulyan <41385173+arulyan@users.noreply.github.com>
2019-08-27 00:33:57 +0530

    Made the articles fetching code simple!

--- FEWNM/src/pages/showarticles/showarticles.ts
+++ FEWNM/src/pages/showarticles/showarticles.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Http } from '@angular/http';
+
 /**
  * Generated class for the ShowarticlePage page.
  *
@@ -13,10 +14,11 @@ import { Http } from '@angular/http';
   selector: 'page-showarticles',
   templateUrl: 'showarticles.html',
 })
+
 export class ShowarticlesPage {
   public: any;
   public myInput: any;
-  public item: any = [];
+  public items: any = [];
   public res: any = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http) {
@@ -29,19 +31,17 @@ export class ShowarticlesPage {
 
   getdata() {
 
-    var i, j;
+    // var i, j;
     this.http.get('http://localhost:3000/getUsers').subscribe(data => {
-      this.item = data.json();
-
-      for (i = 0; i < this.item.length; i++) {
-        this.res.push({
-          title: this.item[i].title,
-          story: this.item[i].story,
-
-        })
-
-
-      }
+      this.items = data.json();
+      // for (i = 0; i < this.item.length; i++) {
+      //   this.res.push({
+      //     title: this.item[i].title,
+      //     story: this.item[i].story,
+      //   })
+      // }
+      // console.log(this.res);
+      console.log(this.items);
     });
   }
 }
